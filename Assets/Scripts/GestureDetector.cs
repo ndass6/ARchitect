372c6250f83a40db24bdb79b046c8c4330d45863
ndass6 <ndass6@gatech.edu>
2016-12-02 18:36:27 -0500

    Copied GestureDetector from previous project

---
+++ Assets/Scripts/GestureDetector.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.VR.WSA.Input;
+
+public class GestureDetector : MonoBehaviour
+{
+    public static GestureDetector Instance { get; private set; }
+
+    GestureRecognizer recognizer;
+    
+    public void Awake()
+    {
+        Instance = this;
+
+        // Set up a GestureRecognizer to detect Select gestures.
+        recognizer = new GestureRecognizer();
+
+        // Executed when a tap event is detected
+        recognizer.TappedEvent += (source, tapCount, ray) =>
+        {
+            // Raycast calculations
+            var headPosition = Camera.main.transform.position;
+            var gazeDirection = Camera.main.transform.forward;
+
+            RaycastHit hitInfo;
+            if (Physics.Raycast(headPosition, gazeDirection, out hitInfo))
+            {
+                // Close card if in line of sight
+                Collider col = hitInfo.collider;
+                if (col.tag == "Card")
+                {
+                    GetComponent<CardController>().RemoveCard(col.GetComponent<Card>());
+                    return;
+                }
+            }
+            else
+            {
+                // TODO Call photo capture instead
+                gameObject.AddComponent<CameraCapture>();
+            }
+        };
+        recognizer.StartCapturingGestures();
+    }
+}
