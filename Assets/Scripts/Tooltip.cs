734b01e1fd31d443f5c640af18cefe2ce61e61c0
ndass6 <ndass6@gatech.edu>
2016-12-03 11:05:07 -0500

    Added Tooltip

---
+++ Assets/Scripts/Tooltip.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Unity;
+using UnityEngine;
+
+public class Tooltip : MonoBehaviour
+{
+    void Update() {
+        RaycastHit hitInfo;
+        Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hitInfo);
+        if (hitInfo.collider.GetComponent<Furnishing>())
+        {
+            showTooltip(hitInfo.collider.gameObject);
+        }
+        else
+        {
+            showTooltip(null);
+        }
+    }
+
+    private void showTooltip(GameObject obj)
+    {
+
+    }
+}
